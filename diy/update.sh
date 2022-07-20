b04157b5ffe0843eb7b603dad102eeb4787525f8
4thanks <63891627+4thanks@users.noreply.github.com>
2022-07-20 16:45:51 +0800

    Update update.sh

--- diy/update.sh
+++ diy/update.sh
@@ -3,7 +3,7 @@
 #backup
 curl -sS https://raw.githubusercontent.com/wy580477/Mosdns-on-PaaS/main/content/config.yaml > /tmp/config-backup.yaml
 
-cp -rf /tmp/*.yaml diy/
+cp -rf /tmp/*.yaml content/diy/
 rm -rf /tmp/*
 
 #rules && Geosite GeoIP && cndbIP data
@@ -11,5 +11,10 @@ curl -sS https://raw.githubusercontent.com/hezhijie0327/CNIPDb/main/cnipdb/count
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat > /tmp/geosite.dat
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat > /tmp/geoip.dat
 
-cp -rf /tmp/*.dat diy/data/
+cp -rf /tmp/*.dat content/diy/
+rm -rf /tmp/*
+
+#gfw_ip list
+curl -sS https://raw.githubusercontent.com/pmkol/easymosdns/rules/gfw_ip_list.txt > /tmp/gfw_ip_list.txt
+cp -rf /tmp/*.txt content/diy/
 rm -rf /tmp/*
